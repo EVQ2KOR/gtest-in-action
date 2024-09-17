e5d27386535f5755aae438c1a47145c3071f802c
EVQ2KOR <devarakondashanthi999@gmail.com>
2024-09-17 17:11:11 +0530

    Update StringCalculatorDataDriven.Tests.cpp

--- StringCalculatorDataDriven.Tests.cpp
+++ StringCalculatorDataDriven.Tests.cpp
@@ -3,35 +3,32 @@
 #include <vector>
 
 class TestDataPair{
-
 public:
     string input;
     int expectedValue;
-    TestDataPair(string _input,int _expectedValue) : input(_input), expectedValue(_expectedValue){
-      
-    }
-}
+    TestDataPair(string _input,int _expectedValue) : input{_input}, expectedValue{_expectedValue}{
+      }
+};
 
-TEST(StringCalculatorDataDrivenTestSuitey,DataDrivenTestCase){
+
+TEST(StringCalculatorDataDrivenTestSuite,DataDrivenTestCase){
   vector<TestDataPair> dataList;
-  TestDataPair Pair_one { "",0};
-  TestDataPair Pair_two { "0",0};
-  TestDataPair Pair_three { "1",1};
-  TestDataPair Pair_four { "1,2",3};
-  TestDataPair Pair_five { "1,2,3",6};
+  TestDataPair pair_one { "",0};
+  TestDataPair pair_two { "0",0};
+  TestDataPair pair_three { "1",1};
+  TestDataPair pair_four { "1,2",3};
+  TestDataPair pair_five { "1,2,3",6};
 
   dataList.push_back(pair_one);
   dataList.push_back(pair_two);
   dataList.push_back(pair_three);
   dataList.push_back(pair_four);
   dataList.push_back(pair_five);
-
-  // Iterate using a range based for loop
-    for (TestDataPair dataPair : deatils){
+  
+  //Iterate using a range-based for-loop
+    for (TestDataPair dataPair : dataList) {
         int actualValue=Add(dataPair.input);
-        ASSERT_EQ(actualValue,dataPair,expectedValue);
-    
+        ASSERT_EQ(actualValue,dataPair.expectedValue);
     }
-        
 
 }
